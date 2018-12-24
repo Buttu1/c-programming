6e975dfcbbfab29c778108c95dbb505508ac2102
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2018-12-24 12:14:07 +0530

    Create set1-1.c

---
+++ set1-1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+{
+printf("positive");
+}
+if(n<0)
+{
+printf("negative");
+}
+if(n==0)
+{
+printf("zero");
+}
+getch();
+}
