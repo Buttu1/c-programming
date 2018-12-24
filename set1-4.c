e5dbcba6f879a8c8576d16b0f98f015304f0da1f
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2018-12-24 12:51:34 +0530

    Create set1-4.c

---
+++ set1-4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+scanf("%c",&c);
+if((c>='A')&&(c<='z'))
+{
+printf("alphabhet");
+}
+else
+{
+printf("not a alphabhet");
+}
+getch();
+}
