5b2e22d398798a6f7b21eb40a18db5b220b9aeca
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2018-12-25 13:17:06 +0530

    Create set1-9.c

---
+++ set1-9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,k,i,a[20],sum=0;
+printf("enter n value");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("enter k value");
+scanf("%d',&k);
+for(j=i;j<=k;j++)
+{
+sum=sum+j;
+}
+printf("the ans is %d",sum);
+getch();
+}
