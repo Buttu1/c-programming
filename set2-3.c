3e3e0d06e244d147062aba8f5e0df682ecb50c28
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2018-12-25 14:12:23 +0530

    Create set2-3.c

---
+++ set2-3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,i;
+scanf("%d",&num);
+for(i=2;i<=n;i++)
+{
+if(num%i!=0)
+{
+printf("%d is prime",num);
+}
+else
+{
+printf("%d not prime",num);
+}
+}
+getch();
+}
