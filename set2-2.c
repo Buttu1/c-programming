6583b0e0f1f8f264e9a36beb4458c4d4a94fba13
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2018-12-25 14:06:14 +0530

    Create set2-2.c

---
+++ set2-2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,rem,temp,sum=0;
+scanf("%d",&num);
+temp=num;
+while(num!=0)
+{
+n=n/10;
+c++;
+}
+num-temp;
+while(num>0)
+{
+rem=num%10
+sum=sum*10+rem;
+num=num/10;
+}
+if(sum==temp)
+{
+printf("palindrome");
+}
+else
+{
+printf("not a ppalindrome");
+}
+getch();
+}
