1a9c5952741253ef4cf609b38d37f29fba7781d1
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2019-01-19 14:41:44 +0530

    Create set3-3.c

---
+++ set3-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[50],n,min,i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+    min=a[0];
+    for(i=0;i<n;i++)
+    {
+    	if(a[i]<min)
+    	{
+    		min=a[i];
+    	}
+    }
+    printf("%d",min);
+
+    return 0;
+}
