6d720678154cec021e68e9933a83a41de7fa8233
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2019-01-27 14:01:22 +0530

    Create p1-8.c

---
+++ p1-8.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[20],b[20];
+int i,j=0,count=0;
+gets(a);
+for(i=0;a[i]!='\0';i++)
+{
+count++;
+}
+for(i=0;i<c;i++)
+{
+if(a[i]>97 &&a[i]==" ")
+{
+b[j]=a[i]-32;
+}
+else
+{
+b[j]=a[i];
+}
+}
+printf("%s",b);
+getch();
+}
