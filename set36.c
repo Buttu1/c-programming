ba07677b1d03e8c553e3f3276bfcfc6665c13e7f
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2019-01-28 13:02:56 +0530

    Create set36.c

---
+++ set36.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+ char a[50];
+ int c=0,i;
+ gets(a);
+ for(i=0;a[i]!='\0';i++)
+ {
+  if(a[i]!='A' && a[i]!='z' && a[i]!=48 && a[i]!=57)
+  {
+   c++;
+  }
+ }
+ printf("%d",c);
+ }
